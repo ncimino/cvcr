8c07ed215c4a35f667c62cfdc64425e372f427b9
Nik <nik.cimino@gmail.com>
2013-05-28 00:23:48 -0600

    removed ckeditor and migrated to using a ckeditor cdn

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,3 +1,3 @@
-var CKEDITOR_BASEPATH = '/assets/ckeditor/';
-//= require ckeditor/init
+var CKEDITOR_BASEPATH = 'http://cdnjs.cloudflare.com/ajax/libs/ckeditor/4.0.1/';
+//= require //cdnjs.cloudflare.com/ajax/libs/ckeditor/4.0.1/ckeditor.js
 //= require active_admin/base
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -140,8 +140,8 @@ ActiveAdmin.setup do |config|
   #
   # To load a javascript file:
   #config.register_javascript 'ckeditor/ckeditor.js'
-  config.register_javascript 'ckeditor/init.js'
-
+  #config.register_javascript 'ckeditor/init.js'
+  config.register_javascript '//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.0.1/ckeditor.js'
 
   # == CSV options
   #
