615cca0ba815888fcfa5b492537e7392f749d9c7
Nik <nik.cimino@gmail.com>
2013-05-27 20:12:39 -0600

    cleaned up the layout a little bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Cvcr::Application.routes.draw do
 
-  get "registrations/edit"
+  get 'registrations/edit'
 
-  get "registrations/new"
+  get 'registrations/new'
 
-  get "authentications/index"
+  get 'authentications/index'
 
-  get "authentications/create"
+  get 'authentications/create'
 
-  get "authentications/destroy"
+  get 'authentications/destroy'
 
-  match '/auth/failure' => 'authentications#failure'
+    match '/auth/failure' => 'authentications#failure'
   match '/auth/:provider/callback' => 'authentications#create'
   
   devise_for :admin_users, ActiveAdmin::Devise.config
@@ -18,9 +18,11 @@ Cvcr::Application.routes.draw do
   devise_for :users, :controllers => {:registrations => 'registrations'}
 
   root :to => 'pages#show', :id => 0
+
   get 'sessions/new'
-#   resources :pages
+  #   resources :pages
   match 'pages/:id' => 'pages#show', :as => :page
 
   ActiveAdmin.routes(self)
+
 end
