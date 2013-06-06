b3b601c0c7a02565d9170f24c0a33bddbff6a5c3
Nik <nik.cimino@gmail.com>
2013-06-06 00:07:25 -0600

    added tileable images and tweaked layout

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,36 @@
-$('.nav li').on('mouseover',function(){
+$('a.nav').hover(function(){
   $('.hidden-menu').show();
+},function(){
+  if ( !$('.hidden-menu:hover'). ) {
+    $('.hidden-menu').hide();
+  };
 });
+
+$('a.nav').on( 'mouseenter mouseleave', function() {
+    $(this).toggleClass('hover');
+  }
+);
+$('div.hidden-menu').on( 'mouseenter mouseleave', function() {
+    $(this).toggleClass('hover');
+  }
+);
+
+if(!$(this).parent().find('ul').first().hasClass('hover')) {
+  $(this).parent().parent().removeClass('open');
+}
+
+//
+//$(function(){
+//
+//  $('*').hover(function(){
+//      $(this).data('hover',1); //store in that element that the mouse is over it
+//    },
+//    function(){
+//      $(this).data('hover',0); //store in that element that the mouse is no longer over it
+//    });
+//
+//
+//  window.isHovering = function (selector) {
+//    return $(selector).data('hover')?true:false; //check element for hover property
+//  }
+//});
