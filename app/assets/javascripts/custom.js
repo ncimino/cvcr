8c6edcdc94d8c348639e3c5888efbbae6a24cbe5
Nik <nik.cimino@gmail.com>
2013-06-07 01:22:13 -0600

    layout logo is slightly overlapping and added example carousel

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,36 +1,42 @@
-$('a.nav').hover(function(){
-  $('.hidden-menu').show();
-},function(){
-  if ( !$('.hidden-menu:hover'). ) {
-    $('.hidden-menu').hide();
-  };
-});
-
-$('a.nav').on( 'mouseenter mouseleave', function() {
-    $(this).toggleClass('hover');
-  }
-);
-$('div.hidden-menu').on( 'mouseenter mouseleave', function() {
-    $(this).toggleClass('hover');
-  }
-);
-
-if(!$(this).parent().find('ul').first().hasClass('hover')) {
-  $(this).parent().parent().removeClass('open');
-}
-
-//
-//$(function(){
-//
-//  $('*').hover(function(){
-//      $(this).data('hover',1); //store in that element that the mouse is over it
-//    },
-//    function(){
-//      $(this).data('hover',0); //store in that element that the mouse is no longer over it
-//    });
-//
-//
-//  window.isHovering = function (selector) {
-//    return $(selector).data('hover')?true:false; //check element for hover property
-//  }
+//$('a.nav').hover(function(){
+//  $('.hidden-menu').show();
+//},function(){
+//  if ( !$('.hidden-menu:hover'). ) {
+//    $('.hidden-menu').hide();
+//  };
 //});
+//
+//$('.dropdown-toggle').on( 'mouseenter', function() {
+//  //alert('hello');
+//  $(this).dropdown();
+//});
+//);
+//$('div.hidden-menu').on( 'mouseenter mouseleave', function() {
+//    $(this).toggleClass('hover');
+//  }
+//);
+//
+//if(!$(this).parent().find('ul').first().hasClass('hover')) {
+//  $(this).parent().parent().removeClass('open');
+//}
+//
+////
+////$(function(){
+////
+////  $('*').hover(function(){
+////      $(this).data('hover',1); //store in that element that the mouse is over it
+////    },
+////    function(){
+////      $(this).data('hover',0); //store in that element that the mouse is no longer over it
+////    });
+////
+////
+////  window.isHovering = function (selector) {
+////    return $(selector).data('hover')?true:false; //check element for hover property
+////  }
+////});
+
+$('.dropdown-toggle').live( 'mouseenter', function() {
+  //alert('hello');
+  $(this).dropdown();
+});
