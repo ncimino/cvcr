0f3bb9a13665014ee99452dc14fe6b1f505a559a
Nik <nik.cimino@gmail.com>
2013-06-16 09:29:13 -0600

    reverted attempted fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,9 +7,6 @@ Cvcr::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
-  config.reload_classes_only_on_change = true
-  config.watchable_dirs[File.join(config.root, "app/admin")] = ["rb"]
-
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
