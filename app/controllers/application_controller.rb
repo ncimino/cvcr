41944c31ed0fd3a0b2d962545a7f5d005112e2d9
Nik <nik.cimino@gmail.com>
2013-07-24 02:11:02 -0600

    fixed bottoms section links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,30 +2,20 @@ class ApplicationController < ActionController::Base
   before_filter :get_variables, :current_cart
 
   def get_variables
-    @areas = MainArea.all
-    if @areas.size > 0
-      @bottom_area = MainArea.find_by_area_name(1)
-      if @bottom_area
-        @bottom_section = @bottom_area.sections.order("ordinal").includes(:products, :pages)
-        if @bottom_section && @bottom_section.first
-          @bottom_pages = @bottom_section.first.pages
-        end
-      end
+    if @bottom_area = MainArea.find_by_area_name(1)
+      @bottom_sections = @bottom_area.sections.order("ordinal").includes(:products, :pages)
     end
-    if @areas.size > 1
-      @area1 = MainArea.find_by_area_name(2)
+    Rails.logger.debug "====================== #{@bottom_area.area_name}"
+    if @area1 = MainArea.find_by_area_name(2)
       @area1_sections = @area1.sections.order("ordinal").includes(:products, :pages)
     end
-    if @areas.size > 2
-      @area2 = MainArea.find_by_area_name(3)
+    if @area2 = MainArea.find_by_area_name(3)
       @area2_sections = @area2.sections.order("ordinal").includes(:products, :pages)
     end
-    if @areas.size > 3
-      @area3 = MainArea.find_by_area_name(4)
+    if @area3 = MainArea.find_by_area_name(4)
       @area3_sections = @area3.sections.order("ordinal").includes(:products, :pages)
     end
-    if @areas.size > 4
-      @area4 = MainArea.find_by_area_name(5)
+    if @area4 = MainArea.find_by_area_name(5)
       @area4_sections = @area4.sections.order("ordinal").includes(:products, :pages)
     end
   end
