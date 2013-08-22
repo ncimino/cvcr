fe8cd7432f00dd8e60137fa82e87d69b5bc793ee
Nik <nik.cimino@gmail.com>
2013-08-22 00:15:30 -0600

    failure is in controller, added catch

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -2,8 +2,12 @@ class PaymentNotificationsController < ApplicationController
   protect_from_forgery :except => [:create]
 
   def create
-    exec("(echo \"Subject: Creating Payment\";echo \"#{params}\") | sendmail -f noreply@#{Rails.application.config.action_mailer.default_url_options[:host]} nik.cimino@gmail.com")
-    PaymentNotification.create!( :params => params, :cart_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id] )
+    begin
+      PaymentNotification.create!( :params => params, :cart_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id] )
+      exec("(echo \"Subject: Created Payment\";echo \"#{params}\") | sendmail -f noreply@#{Rails.application.config.action_mailer.default_url_options[:host]} nik.cimino@gmail.com")
+    rescue Exception => exc
+      exec("(echo \"Subject: FAILED Create Payment\";echo\"#{exc.message}\";echo \"#{params}\") | sendmail -f noreply@#{Rails.application.config.action_mailer.default_url_options[:host]} nik.cimino@gmail.com")
+    end
     render :nothing => true
   end
 
